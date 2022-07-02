d8ba64171bdfe1fc342511034cbe7efeed99dd47
Diego <diego.rruy@hotmail.com>
2022-07-02 18:58:26 -0300

    darkroom-assessment: create cart cart_item product and order structures

---
+++ db/migrate/20220702211358_create_orders.rb
@@ -0,0 +1,10 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.string :address
+      t.string :payment_method
+
+      t.timestamps
+    end
+  end
+end
