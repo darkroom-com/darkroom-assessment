3a7a2fc4d9293661991dc0504f7e8eb5a45958dd
Diego <diego.rruy@hotmail.com>
2022-07-02 22:06:34 -0300

    darkroom-assessment: create tests to itens added in cart

--- test/models/cart_item_test.rb
+++ test/models/cart_item_test.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 require "test_helper"
 
 class CartItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should validate cart item" do
+    cart_item = CartItem.new(cart_id: 1, product_id: 1)
+
+    assert cart_item.valid?
+  end
 end
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,17 @@
+# frozen_string_literal: true
+
 require "test_helper"
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save cart" do
+    cart = Cart.new(guest_id: '40647c8d85af9567408ed1e132091e88f4e6c3a4b48c0313a59045caf2b735fb')
+
+    assert cart.save
+  end
+
+  test "should not accept repeated guest_id" do
+    cart = Cart.new(guest_id: '37d7a80604871e579850a658c7add2ae7557d0c6abcc9b31ecddc4424207eba3')
+
+    assert_not cart.valid?
+  end
 end
